6046229a0a988a86f328b3094429cf4f34cb93d6
D2Allaire <info@d2am.net>
2016-04-11 14:00:08 +0200

    Basic Algorithm Scripting P2

--- Basic Algorithm Scripting.js
+++ Basic Algorithm Scripting.js
@@ -3,14 +3,14 @@
  */
 
 function reverseString(str) {
-  var temp = '';
-  for (var i=(str.length-1); i>=0; i--) {
-    temp += str[i];
-  }
-  return temp;
+    var temp = '';
+    for (var i = (str.length - 1); i >= 0; i--) {
+        temp += str[i];
+    }
+    return temp;
 }
 
-reverseString("hello");
+reverseString("hello"); // --> "olleh"
 
 
 /**
@@ -18,14 +18,14 @@ reverseString("hello");
  */
 
 function factorialize(num) {
-  if (num === 0) {
-    return 1;
-  } else {
-    return num*factorialize(num-1);
-  }
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * factorialize(num - 1);
+    }
 }
 
-factorialize(5);
+factorialize(5); // --> 5*4*3*2*1=120
 
 
 /**
@@ -33,18 +33,18 @@ factorialize(5);
  */
 
 function palindrome(str) {
-  str = str.toLowerCase();
-  str = str.replace(/[^a-z\d]/gi, "");
-  strtemp = "";
-  for (var i=str.length-1; i>=0; i--){
-    strtemp += str[i];
-  }
-  
-  if (strtemp === str) return true;
-  else return false;
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z\d]/gi, "");
+    strtemp = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        strtemp += str[i];
+    }
+
+    if (strtemp === str) return true;
+    else return false;
 }
 
-palindrome("eye");
+palindrome("eye"); // --> true
 
 
 /**
@@ -52,15 +52,15 @@ palindrome("eye");
  */
 
 function findLongestWord(str) {
-  var strarray = str.split(" ");
-  var max = 0;
-  for (var i=0; i<strarray.length; i++) {
-    if (strarray[i].length > max) max = strarray[i].length;
-  }
-  return max;
+    var strarray = str.split(" ");
+    var max = 0;
+    for (var i = 0; i < strarray.length; i++) {
+        if (strarray[i].length > max) max = strarray[i].length;
+    }
+    return max;
 }
 
-findLongestWord("The quick brown fox jumped over the lazy dog");
+findLongestWord("The quick brown fox jumped over the lazy dog"); // --> 6
 
 
 /**
@@ -68,16 +68,16 @@ findLongestWord("The quick brown fox jumped over the lazy dog");
  */
 
 function titleCase(str) {
-  str = str.toLowerCase();
-  var strings = str.split(" ");
-  var res = [];
-  for (var i=0; i<strings.length; i++) {
-    res[i] = strings[i].charAt(0).toUpperCase() + strings[i].slice(1);
-  }
-  return res.join(" ");
+    str = str.toLowerCase();
+    var strings = str.split(" ");
+    var res = [];
+    for (var i = 0; i < strings.length; i++) {
+        res[i] = strings[i].charAt(0).toUpperCase() + strings[i].slice(1);
+    }
+    return res.join(" ");
 }
 
-titleCase("I'm a little tea pot");
+titleCase("I'm a little tea pot"); // --> "I'm A Little Tea Pot"
 
 
 /**
@@ -85,18 +85,24 @@ titleCase("I'm a little tea pot");
  */
 
 function largestOfFour(arr) {
-  var res = [];
-  for (var i=0; i<arr.length; i++) {
-      var max = 0;
-      for (var j=0; j<arr[i].length; j++) {
-          if (arr[i][j] > max) max = arr[i][j];
-      }
-      res[i] = max;
-  }
-  return res;
+    var res = [];
+    for (var i = 0; i < arr.length; i++) {
+        var max = 0;
+        for (var j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > max) max = arr[i][j];
+        }
+        res[i] = max;
+    }
+    return res;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+]);
+// --> [5, 27, 39, 1001]
 
 
 /**
@@ -104,19 +110,19 @@ largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 85
  */
 
 function confirmEnding(str, target) {
-  str = str.toLowerCase();
-  target = target.toLowerCase();
-  var res = "";
-  for (var i=0; i<target.length; i++) {
-      if (str[str.length-i-1] === target[target.length-i-1])
-      res += target[target.length-i-1];
-  }
-  res = res.split('').reverse().join('');
-  if (res === target) return true;
-  else return false;
+    str = str.toLowerCase();
+    target = target.toLowerCase();
+    var res = "";
+    for (var i = 0; i < target.length; i++) {
+        if (str[str.length - i - 1] === target[target.length - i - 1])
+            res += target[target.length - i - 1];
+    }
+    res = res.split('').reverse().join('');
+    if (res === target) return true;
+    else return false;
 }
 
-confirmEnding("Bastian", "n");
+confirmEnding("Bastian", "n"); // --> true
 
 
 /**
@@ -124,12 +130,156 @@ confirmEnding("Bastian", "n");
  */
 
 function repeatStringNumTimes(str, num) {
-  var initstr = str;
-  if (num<0) return "";
-  for (var i=0; i<num-1; i++) {
-    str += initstr;
-  }
-  return str;
+    var initstr = str;
+    if (num < 0) return "";
+    for (var i = 0; i < num - 1; i++) {
+        str += initstr;
+    }
+    return str;
 }
 
-repeatStringNumTimes("abc", 3);
+repeatStringNumTimes("abc", 3); // --> "abcabcabc"
+
+
+/**
+ * Truncate a string.
+ */
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        if (num < 3) {
+            return str.replace(str.slice(num), '') + "...";
+        } else {
+            return str.replace(str.slice(num - 3), '') + "...";
+        }
+    } else return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+
+/**
+ * Split array into equally sized groups
+ */
+
+function chunkArrayInGroups(arr, size) {
+    var slicedArray = [];
+    var step = 0;
+    for (var i=0; i<arr.length/size; i++) {
+        if (arr.length > step+size) {
+            slicedArray[i] = arr.slice(step, step+size);
+            step += size;
+        } else {
+            slicedArray[i] = arr.slice(step);
+        }
+    }
+    return slicedArray;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d", "e"], 2); // -> [["a", "b"]["c", "d"], ["e"]]
+
+
+/**
+ * Return remaining elements of an array, after chopping off n elements from the start.
+ */
+
+function slasher(arr, howMany) {
+    if (howMany > arr.length) {
+        return [];
+    } else {
+        arr.splice(0, howMany);
+        return arr;
+    }
+}
+
+slasher(["a", "b", "c"], 2); // -> ["c"]
+
+
+/**
+ * Return true if the string in the first element of the array contains all of the letters of 
+ * the string in the second element of the array.
+ */
+
+function mutation(arr) {
+    var letters = arr[1].toLowerCase().split('');
+    for (var i=0; i<letters.length; i++) {
+        if (arr[0].toLowerCase().indexOf(letters[i]) < 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+mutation(["hello", "hey"]); // ->  false
+
+
+/**
+ * Remove all falsy values from an array.
+ */
+
+function bouncer(arr) {
+    var filtered = arr.filter(function(value) {
+        if (value) return true;
+        else return false;
+    });
+    return filtered;
+}
+
+bouncer([7, "ate", "", false, 9]); // -> [7, "ate", 9]
+
+
+/**
+ * Delete all elements provided by function argument from array.
+ */
+
+function destroyer(arr) {
+    var args = Array.prototype.slice.call(arguments);
+    args = args.slice(1);
+    function destroy(value) {
+        if (args.indexOf(value) >= 0) return false;
+        else return true;
+    }
+    return arr.filter(destroy);
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3); // -> [1, 1]
+
+
+/**
+ * Return lowest index at which a value should be inserted into array (sorted).
+ */
+
+function getIndexToIns(arr, num) {
+    arr = arr.sort(function (a, b) {
+        return a - b;
+    });
+    for (var i=0; i<arr.length; i++) {
+        if (num <= arr[i]) return i;
+    }
+    return arr.length;
+}
+
+getIndexToIns([40, 60], 50); // -> 1
+
+
+/**
+ * Caesar cipher (ROT13).
+ */
+
+function rot13(str) {
+    var codes = [];
+    for (var i=0; i<str.length; i++) {
+        var code = str[i].charCodeAt();
+        if ((code > 77 && code < 91) || (code > 109 && code < 123)) {
+            codes[i] = code - 13;
+        } else if ((code > 64 && code < 78) || (code > 96 && code < 110)) {
+            codes[i] = code + 13;
+        } else {
+            codes[i] = code;
+        }
+    }
+    var res = String.fromCharCode.apply(null, codes);
+    return res;
+}
+
+rot13("SERR PBQR PNZC"); // -> "FREE CODE CAMP"
